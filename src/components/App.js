50bddf21686e2992ab8a4ab633a41043d2be0d96
afonsobarros <a@ignited.io>
2017-08-11 15:52:53 +0100

    move app to components. create home view

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Home from '../views/Home';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello World
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/views/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        Home
+        <i className="material-icons">android</i>
+      </div>
+    );
+  }
+}
+
+export default Home;
