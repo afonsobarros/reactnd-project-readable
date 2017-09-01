779d79066c5070335d43820e91301bb8349f8dcb
afonsobarros <a@ignited.io>
2017-09-01 16:02:39 +0100

    indentation

--- src/index.js
+++ src/index.js
@@ -8,28 +8,27 @@ import reducer from './reducers'
 import { Provider } from 'react-redux'
 
 const logger = store => next => action => {
-    console.group(action.type)
-    console.info('dispatching', action)
-    let result = next(action)
-    console.log('next state', store.getState())
-    console.groupEnd(action.type)
-    return result
-  }
-  
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-  
-  const store = createStore(
-    reducer,
-    composeEnhancers(
-      applyMiddleware(logger)
-    )
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd(action.type)
+  return result
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(logger)
   )
-  
+)
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
 registerServiceWorker();
